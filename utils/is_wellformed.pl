f657c680210cba5216e87bddbe69f763edfb0f26
Kaarel Kaljurand <kaljurand@gmail.com>
2012-01-31 22:08:30 +0100

    Replace `howm` by `how_many` or `how_much`

--- utils/is_wellformed.pl
+++ utils/is_wellformed.pl
@@ -130,7 +130,7 @@ is_condition(query(R, Lemma)-Id) :-
 	id(Id),
 	var(R),
 	nonvar(Lemma),
-	member(Lemma, [who, what, which, how, where, when, howm]).
+	member(Lemma, [who, what, which, how, where, when, how_many, how_much]).
 
 is_condition(relation(R1, of, R2)-Id) :-
 	id(Id),
