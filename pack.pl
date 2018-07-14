7bac22f880d65fa4873e268480982687d9ccceb7
Kaarel Kaljurand <kaljurand@gmail.com>
2018-07-15 00:21:01 +0200

    pack.pl: Add authors' email addresses
    Otherwise installation fails

--- pack.pl
+++ pack.pl
@@ -1,13 +1,13 @@
 name(ape).
-version('6.7.180714').
+version('6.7.180715').
 
 % see http://www.swi-prolog.org/howto/PackInfo.html
 
 download('https://github.com/Attempto/APE/releases/*.zip').
 
-title('Parser for Attempto Controlled English (ACE) ').
-author('Kaarel Kaljurand','(tbd)').
-author('Norbert E. Fuchs','(tbd)').
-author('Tobias Kuhn','(tbd)').
+title('Parser for Attempto Controlled English (ACE)').
+author('Kaarel Kaljurand','kaljurand@gmail.com').
+author('Norbert E. Fuchs','fuchs@ifi.uzh.ch').
+author('Tobias Kuhn','kuhntobias@gmail.com').
 
 home('https://github.com/Attempto/APE').
