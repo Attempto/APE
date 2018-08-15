b93fc120f638ba21ae3ef89a66003d8e1d466762
Kaarel Kaljurand <kaljurand@gmail.com>
2018-08-15 23:57:05 +0300

    Update Windows build script (after merging #24)

--- make_exe.bat
+++ make_exe.bat
@@ -1,6 +1,6 @@
 @echo off
 del ape.exe
-swipl -O -F none -g "working_directory(_, 'parser'), [fit_to_plp], halt." -t halt & swipl -O -f ape.pl -g "qsave_program('ape.exe', [goal(ape), toplevel(halt)])." -t halt
+swipl -O -F none -g "working_directory(_, 'prolog/parser'), [fit_to_plp], halt." -t halt & swipl -O -f ape.pl -g "qsave_program('ape.exe', [goal(ape), toplevel(halt)])." -t halt
 if exist ape.exe goto success
 echo Could not find the SWI Prolog command.
 echo Make sure that the location of the SWI Prolog binaries is in the PATH variable.
